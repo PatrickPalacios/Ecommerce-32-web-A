77c15e50eda5f19b1b5dc36a175b8ceeaf814133
Patrick <patriciopalaciosrivera@gmail.com>
2024-10-20 01:27:51 -0600

    Creamos los datos para los productos

---
+++ ecommerce/utils/data.js
@@ -0,0 +1,61 @@
+export const data = {
+    products: [
+        {
+        id: 1,
+        title: 'Product 1',
+        description: 'Porduct 1 description',
+        price:  100,
+        image
+        },
+         {
+        id: 2,
+        title: 'Product 2',
+        description: 'Porduct 2 description',
+        price: 200,
+        image
+        },
+         {
+        id: 3,
+        title: 'Product 3',
+        description: 'Porduct 3 description',
+        price: 300,
+        image
+        },
+         {
+        id: 4,
+        title: 'Product 4',
+        description: 'Porduct 4 description',
+        price: 400,
+        image
+        },
+         {
+        id: 5,
+        title: 'Product 5',
+        description: 'Porduct 5 description',
+        price: 500,
+        image
+        },
+         {
+        id: 6,
+        title: 'Product 6',
+        description: 'Porduct 6 description',
+        price: 600,
+        image
+        },
+         {
+        id: 7,
+        title: 'Product 7',
+        description: 'Porduct 7 description',
+        price: 700,
+        image
+        },
+         {
+        id: 8,
+        title: 'Product 8',
+        description: 'Porduct 8 description',
+        price: 800,
+        image
+        },
+    ]
+}
+    
